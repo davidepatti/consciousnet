98b5f875d4dcaf64f864e199ff731669a53c67dc
xedivad@gmail.com <xedivad@gmail.com@f2855747-ca4b-1b50-c46b-748f74577288>
2014-11-12 22:32:34 +0000

    tsss

---
+++ tmp/Data.pm
@@ -0,0 +1,8 @@
+package Data 1.0001;
+use parent qw(Exporter);
+
+our @EXPORT = qw($api_key $cx);
+
+our $api_key = 'AIzaSyAOZZHHWpMWo_cNt_iyCsmvLMa_XIEvofU';
+our $cx      = '002983251636507551537:jfswnqh-cd8';
+
