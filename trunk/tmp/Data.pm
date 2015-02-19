551aec37c0ad5e65ab4ad85ec099609a03b64dc8
salvatoremonteleone@gmail.com <salvatoremonteleone@gmail.com@f2855747-ca4b-1b50-c46b-748f74577288>
2015-02-19 13:26:05 +0000

    Remove temporary data folder

--- trunk/tmp/Data.pm
+++
@@ -1,8 +0,0 @@
-package Data 1.0001;
-use parent qw(Exporter);
-
-our @EXPORT = qw($api_key $cx);
-
-our $api_key = 'AIzaSyAOZZHHWpMWo_cNt_iyCsmvLMa_XIEvofU';
-our $cx      = '002983251636507551537:jfswnqh-cd8';
-
