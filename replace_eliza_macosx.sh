232fd3bb87067225a64b3c17541a087b497d6499
Davide Patti <xedivad@gmail.com>
2016-07-22 12:55:40 +0200

    Updates paths

--- replace_eliza_macosx.sh
+++ replace_eliza_macosx.sh
@@ -1,2 +1,4 @@
-echo "sudo cp Eliza.pm /opt/local/lib/perl5/site_perl/5.16.3/Chatbot"
+echo "trying: sudo cp Eliza.pm /opt/local/lib/perl5/site_perl/5.16.3/Chatbot"
 sudo cp Eliza.pm /opt/local/lib/perl5/site_perl/5.16.3/Chatbot
+echo "trying: sudo cp Eliza.pm /Library/Perl/5.18/Chatbot/Eliza.pm"
+sudo cp Eliza.pm /Library/Perl/5.18/Chatbot/Eliza.pm 
